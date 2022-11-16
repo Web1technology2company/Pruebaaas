866ee3b0440cf6b913ce6b4822e1f13837b0dbf4
ST-Lanus-2022 <106210265+Web1technology2company@users.noreply.github.com>
2022-11-16 19:32:06 -0300

    Add files via upload

---
+++ assets/js/menu.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('.nav');
+const menu_btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+
+
+    menu_btn.addEventListener('click', () => {
+        menu.classList.toggle('active')
+    })
+
